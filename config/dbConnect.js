d55fd60d33eb55c7f6f939a10fdb9dbba7a51b8d
eggzuxi <eggzuxi99@gmail.com>
2024-03-08 12:04:08 +0900

    env_update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const dbConnect = async () => {
   try{
-    const connect = await mongoose.connect(process.env.DB_LOCAL_URL);
+    const connect = await mongoose.connect(process.env.DB_ATLAS_URL);
     console.log("DB connect");
   }catch(err){
     console.log(err);
